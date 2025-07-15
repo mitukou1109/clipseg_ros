e9b4d07e4cd5ae486dd336ff7ed640e60a6b9aa8
mitukou1109 <mitukou1109@gmail.com>
2025-07-15 05:16:17 +0000

    install sample parameter file

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/config", ["config/segmentation.yaml"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
